1c9184f385c560fdf380a3042c281a19db2e982d
jgeary <jgeary@users.noreply.github.com>
2022-12-21 16:21:57 -0500

    feat: initial commit

---
+++ src/governance/RecoveryTreasury.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "@openzeppelin-upgradeable/contracts/governance/TimelockControllerUpgradeable.sol";
+
+contract RecoveryTreasury is TimelockControllerUpgradeable {
+    function __RecoveryTreasury_init(
+        uint256 _minDelay,
+        address[] memory _proposers,
+        address[] memory _executors,
+        address _admin
+    ) public initializer {
+        __TimelockController_init(_minDelay, _proposers, _executors, _admin);
+    }
+}
