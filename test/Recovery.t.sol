c71b55f17f730b8541e61e42c26e601ab2217227
James Geary <36774175+jgeary@users.noreply.github.com>
2023-04-06 13:26:26 -0400

    done

--- test/Recovery.t.sol
+++ test/Recovery.t.sol
@@ -8,7 +8,6 @@ import { EIP712Upgradeable } from "@openzeppelin-upgradeable/contracts/utils/cry
 
 import { RecoveryProxy } from "../src/upgradeability/RecoveryProxy.sol";
 import { RecoveryRegistry } from "../src/RecoveryRegistry.sol";
-import { VoteAggregator } from "../src/VoteAggregator.sol";
 import { RecoveryCollection } from "../src/token/RecoveryCollection.sol";
 import { RecoveryGovernor } from "../src/governance/RecoveryGovernor.sol";
 import { RecoveryTreasury } from "../src/governance/RecoveryTreasury.sol";
@@ -18,7 +17,6 @@ contract RecoveryTest is Test, EIP712Upgradeable {
     RecoveryRegistry registry;
     MockVoting721 tomb;
     MockVoting721 indexMarker;
-    VoteAggregator aggregator;
 
     bytes32 public constant BALLOT_TYPEHASH = keccak256("Ballot(uint256 proposalId,uint8 support)");
 
@@ -29,7 +27,6 @@ contract RecoveryTest is Test, EIP712Upgradeable {
     uint256 voterPrivateKey;
 
     function setUp() external {
-        aggregator = new VoteAggregator();
         address collectionImpl = address(new RecoveryCollection());
         address governorImpl = address(new RecoveryGovernor());
         address treasuryImpl = address(new RecoveryTreasury());
@@ -114,69 +111,69 @@ contract RecoveryTest is Test, EIP712Upgradeable {
         assertEq(collection.tokenURI(1), "https://test.com");
     }
 
-    function testVotingBySig() external {
-        vm.prank(tombDeployer);
-        registry.registerParentCollection(
-            address(tomb),
-            address(indexMarker),
-            address(0),
-            1,
-            50400,
-            172800,
-            1,
-            0,
-            false,
-            true,
-            10
-        );
+    // function testVotingBySig() external {
+    //     vm.prank(tombDeployer);
+    //     registry.registerParentCollection(
+    //         address(tomb),
+    //         address(indexMarker),
+    //         address(0),
+    //         1,
+    //         50400,
+    //         172800,
+    //         1,
+    //         0,
+    //         false,
+    //         true,
+    //         10
+    //     );
 
-        vm.prank(tombHolder);
-        registry.createRecoveryCollectionForParentToken(address(tomb), 0, address(indexMarker));
+    //     vm.prank(tombHolder);
+    //     registry.createRecoveryCollectionForParentToken(address(tomb), 0, address(indexMarker));
 
-        RecoveryRegistry.RecoveryCollectionAddresses memory addresses = registry.getRecoveryAddressesForParentToken(
-            address(tomb),
-            0
-        );
-        RecoveryCollection collection = RecoveryCollection(addresses.collection);
-        RecoveryGovernor governor = RecoveryGovernor(addresses.governor);
-        RecoveryTreasury treasury = RecoveryTreasury(addresses.treasury);
+    //     RecoveryRegistry.RecoveryCollectionAddresses memory addresses = registry.getRecoveryAddressesForParentToken(
+    //         address(tomb),
+    //         0
+    //     );
+    //     RecoveryCollection collection = RecoveryCollection(addresses.collection);
+    //     RecoveryGovernor governor = RecoveryGovernor(addresses.governor);
+    //     RecoveryTreasury treasury = RecoveryTreasury(addresses.treasury);
 
-        address[] memory targets = new address[](1);
-        targets[0] = address(collection);
-        uint256[] memory values = new uint256[](1);
-        bytes[] memory calldatas = new bytes[](1);
-        calldatas[0] = abi.encodeWithSignature("safeMint(address,string)", tombHolder, "https://test.com");
-        vm.startPrank(tombHolder);
-        uint256 proposalId = governor.propose(targets, values, calldatas, "");
-        vm.roll(block.number + 2);
+    //     address[] memory targets = new address[](1);
+    //     targets[0] = address(collection);
+    //     uint256[] memory values = new uint256[](1);
+    //     bytes[] memory calldatas = new bytes[](1);
+    //     calldatas[0] = abi.encodeWithSignature("safeMint(address,string)", tombHolder, "https://test.com");
+    //     vm.startPrank(tombHolder);
+    //     uint256 proposalId = governor.propose(targets, values, calldatas, "");
+    //     vm.roll(block.number + 2);
 
-        address[] memory _contracts = new address[](1);
-        _contracts[0] = address(governor);
-        uint256[] memory _proposalIds = new uint256[](1);
-        _proposalIds[0] = proposalId;
-        uint8[] memory _supportOptions = new uint8[](1);
-        _supportOptions[0] = uint8(GovernorCountingSimple.VoteType.For);
+    //     address[] memory _contracts = new address[](1);
+    //     _contracts[0] = address(governor);
+    //     uint256[] memory _proposalIds = new uint256[](1);
+    //     _proposalIds[0] = proposalId;
+    //     uint8[] memory _supportOptions = new uint8[](1);
+    //     _supportOptions[0] = uint8(GovernorCountingSimple.VoteType.For);
 
-        (uint8 v, bytes32 r, bytes32 s) = voteSignature(
-            voterPrivateKey,
-            proposalId,
-            uint8(GovernorCountingSimple.VoteType.For)
-        );
-        uint8[] memory _v = new uint8[](1);
-        _v[0] = v;
-        bytes32[] memory _r = new bytes32[](1);
-        _r[0] = r;
-        bytes32[] memory _s = new bytes32[](1);
-        _s[0] = s;
-        aggregator.castVotes(_contracts, _proposalIds, _supportOptions, _v, _r, _s);
-    }
+    //     (uint8 v, bytes32 r, bytes32 s) = voteSignature(
+    //         voterPrivateKey,
+    //         proposalId,
+    //         uint8(GovernorCountingSimple.VoteType.For)
+    //     );
+    //     uint8[] memory _v = new uint8[](1);
+    //     _v[0] = v;
+    //     bytes32[] memory _r = new bytes32[](1);
+    //     _r[0] = r;
+    //     bytes32[] memory _s = new bytes32[](1);
+    //     _s[0] = s;
+    //     aggregator.castVotes(_contracts, _proposalIds, _supportOptions, _v, _r, _s);
+    // }
 
-    function voteSignature(uint256 privateKey, uint256 proposalId, uint8 support)
-        internal
-        returns (uint8 v, bytes32 r, bytes32 s)
-    {
-        bytes32 h = _hashTypedDataV4(keccak256(abi.encode(BALLOT_TYPEHASH, proposalId, support)));
+    // function voteSignature(uint256 privateKey, uint256 proposalId, uint8 support)
+    //     internal
+    //     returns (uint8 v, bytes32 r, bytes32 s)
+    // {
+    //     bytes32 h = _hashTypedDataV4(keccak256(abi.encode(BALLOT_TYPEHASH, proposalId, support)));
 
-        return vm.sign(privateKey, h);
-    }
+    //     return vm.sign(privateKey, h);
+    // }
 }
